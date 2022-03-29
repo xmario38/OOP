1f7452130505fbb854b9bb16de2d02967b37b771
Dylan <54340284+xmario38@users.noreply.github.com>
2022-03-29 14:15:30 +0700

    Add files via upload

---
+++ Dylan Desaffa Zulkarnaen_OOP/OOP.py
@@ -0,0 +1,347 @@
+def garis():
+    print("--------------------")
+
+print("Program Menginap Di Hotel")
+print("-------------------------------------------------------------------")
+print("                :superior        :deluxe          :premium ")
+print("-------------------------------------------------------------------")
+print("1 s.d 2 hari    :100,000/night   :150.000/night   :200.000/night")
+print("3 s.d 4 hari    :90.000/night    :135.000/night   :180.000/night")
+print("5 hari keatas   :80.000/night    :120.000/night   :160.000/night")
+print("-------------------------------------------------------------------")
+
+print("tema kamar")
+print("1.superior")
+print("2.deluxe")
+print("3.premium")
+tipe_kamar = input("masukan tipe kamar : ")
+lama_inap = int(input("masukan lama menginap : "))
+
+if tipe_kamar == "1":
+    if lama_inap <= 2:
+        total_harga = 100000*lama_inap
+    elif lama_inap <= 4:
+        total_harga = 90000*lama_inap
+    else:
+        total_harga = 80000*lama_inap
+
+elif tipe_kamar == "2":
+      if lama_inap <= 2:
+          total_harga = 150000*lama_inap
+      elif lama_inap <= 4:
+          total_harga = 135000*lama_inap
+      else:
+          total_harga = 120000*lama_inap
+
+elif tipe_kamar == "3":
+     if lama_inap <= 2:
+         total_harga = 200000*lama_inap
+     elif lama_inap <= 4:
+         total_harga = 180000*lama_inap
+     else:
+         total_harga = 160000*lama_inap
+
+garis()
+print(" tipe kamar yang di pilih : ", (tipe_kamar))
+print(" lama menginap : ", (lama_inap), " hari")
+print(" total harga yang di bayar : rp. ", (total_harga))
+
+home = input("ingin memesan lagi? (Y/N) : ")
+back = print("")
+if home == "Y":
+    garis()
+    tipe_kamar = input("masukan tipe kamar : ")
+    lama_inap = int(input("masukan lama menginap : "))
+
+    if lama_inap <= 2:
+        total_harga = 100000*lama_inap
+    elif lama_inap <= 4:
+        total_harga = 90000*lama_inap
+    else:
+        total_harga = 80000*lama_inap
+
+    if lama_inap <= 2:
+          total_harga = 150000*lama_inap
+    elif lama_inap <= 4:
+          total_harga = 135000*lama_inap
+    else:
+          total_harga = 120000*lama_inap
+
+    if lama_inap <= 2:
+         total_harga = 200000*lama_inap
+    elif lama_inap <= 4:
+         total_harga = 180000*lama_inap
+    else:
+         total_harga = 160000*lama_inap
+
+    garis()
+    print(" tipe kamar yang di pilih : ", (tipe_kamar))
+    print(" lama menginap : ", (lama_inap), " hari")
+    print(" total harga yang di bayar : rp. ", (total_harga))
+    home = input("ingin memesan lagi? (Y/N) : ")
+
+back = print("")
+if home == "Y":
+    garis()
+    tipe_kamar = input("masukan tipe kamar : ")
+    lama_inap = int(input("masukan lama menginap : "))
+
+    if lama_inap <= 2:
+        total_harga = 100000*lama_inap
+    elif lama_inap <= 4:
+        total_harga = 90000*lama_inap
+    else:
+        total_harga = 80000*lama_inap
+
+    if lama_inap <= 2:
+          total_harga = 150000*lama_inap
+    elif lama_inap <= 4:
+          total_harga = 135000*lama_inap
+    else:
+          total_harga = 120000*lama_inap
+
+    if lama_inap <= 2:
+         total_harga = 200000*lama_inap
+    elif lama_inap <= 4:
+         total_harga = 180000*lama_inap
+    else:
+         total_harga = 160000*lama_inap
+
+    garis()
+    print(" tipe kamar yang di pilih : ", (tipe_kamar))
+    print(" lama menginap : ", (lama_inap), " hari")
+    print(" total harga yang di bayar : rp. ", (total_harga))
+    home = input("ingin memesan lagi? (Y/N) : ")
+
+back = print("")
+if home == "Y":
+    garis()
+    tipe_kamar = input("masukan tipe kamar : ")
+    lama_inap = int(input("masukan lama menginap : "))
+
+    if lama_inap <= 2:
+        total_harga = 100000*lama_inap
+    elif lama_inap <= 4:
+        total_harga = 90000*lama_inap
+    else:
+        total_harga = 80000*lama_inap
+
+    if lama_inap <= 2:
+          total_harga = 150000*lama_inap
+    elif lama_inap <= 4:
+          total_harga = 135000*lama_inap
+    else:
+          total_harga = 120000*lama_inap
+
+    if lama_inap <= 2:
+         total_harga = 200000*lama_inap
+    elif lama_inap <= 4:
+         total_harga = 180000*lama_inap
+    else:
+         total_harga = 160000*lama_inap
+
+    garis()
+    print(" tipe kamar yang di pilih : ", (tipe_kamar))
+    print(" lama menginap : ", (lama_inap), " hari")
+    print(" total harga yang di bayar : rp. ", (total_harga))
+    home = input("ingin memesan lagi? (Y/N) : ")
+
+back = print("")
+if home == "Y":
+    garis()
+    tipe_kamar = input("masukan tipe kamar : ")
+    lama_inap = int(input("masukan lama menginap : "))
+
+    if lama_inap <= 2:
+        total_harga = 100000*lama_inap
+    elif lama_inap <= 4:
+        total_harga = 90000*lama_inap
+    else:
+        total_harga = 80000*lama_inap
+
+    if lama_inap <= 2:
+          total_harga = 150000*lama_inap
+    elif lama_inap <= 4:
+          total_harga = 135000*lama_inap
+    else:
+          total_harga = 120000*lama_inap
+
+    if lama_inap <= 2:
+         total_harga = 200000*lama_inap
+    elif lama_inap <= 4:
+         total_harga = 180000*lama_inap
+    else:
+         total_harga = 160000*lama_inap
+
+    garis()
+    print(" tipe kamar yang di pilih : ", (tipe_kamar))
+    print(" lama menginap : ", (lama_inap), " hari")
+    print(" total harga yang di bayar : rp. ", (total_harga))
+    home = input("ingin memesan lagi? (Y/N) : ")
+
+back = print("")
+if home == "Y":
+    garis()
+    tipe_kamar = input("masukan tipe kamar : ")
+    lama_inap = int(input("masukan lama menginap : "))
+
+    if lama_inap <= 2:
+        total_harga = 100000*lama_inap
+    elif lama_inap <= 4:
+        total_harga = 90000*lama_inap
+    else:
+        total_harga = 80000*lama_inap
+
+    if lama_inap <= 2:
+          total_harga = 150000*lama_inap
+    elif lama_inap <= 4:
+          total_harga = 135000*lama_inap
+    else:
+          total_harga = 120000*lama_inap
+
+    if lama_inap <= 2:
+         total_harga = 200000*lama_inap
+    elif lama_inap <= 4:
+         total_harga = 180000*lama_inap
+    else:
+         total_harga = 160000*lama_inap
+
+    garis()
+    print(" tipe kamar yang di pilih : ", (tipe_kamar))
+    print(" lama menginap : ", (lama_inap), " hari")
+    print(" total harga yang di bayar : rp. ", (total_harga))
+    home = input("ingin memesan lagi? (Y/N) : ")
+
+back = print("")
+if home == "Y":
+    garis()
+    tipe_kamar = input("masukan tipe kamar : ")
+    lama_inap = int(input("masukan lama menginap : "))
+
+    if lama_inap <= 2:
+        total_harga = 100000*lama_inap
+    elif lama_inap <= 4:
+        total_harga = 90000*lama_inap
+    else:
+        total_harga = 80000*lama_inap
+
+    if lama_inap <= 2:
+          total_harga = 150000*lama_inap
+    elif lama_inap <= 4:
+          total_harga = 135000*lama_inap
+    else:
+          total_harga = 120000*lama_inap
+
+    if lama_inap <= 2:
+         total_harga = 200000*lama_inap
+    elif lama_inap <= 4:
+         total_harga = 180000*lama_inap
+    else:
+         total_harga = 160000*lama_inap
+
+    garis()
+    print(" tipe kamar yang di pilih : ", (tipe_kamar))
+    print(" lama menginap : ", (lama_inap), " hari")
+    print(" total harga yang di bayar : rp. ", (total_harga))
+    home = input("ingin memesan lagi? (Y/N) : ")
+
+back = print("")
+if home == "Y":
+    garis()
+    tipe_kamar = input("masukan tipe kamar : ")
+    lama_inap = int(input("masukan lama menginap : "))
+
+    if lama_inap <= 2:
+        total_harga = 100000*lama_inap
+    elif lama_inap <= 4:
+        total_harga = 90000*lama_inap
+    else:
+        total_harga = 80000*lama_inap
+
+    if lama_inap <= 2:
+          total_harga = 150000*lama_inap
+    elif lama_inap <= 4:
+          total_harga = 135000*lama_inap
+    else:
+          total_harga = 120000*lama_inap
+
+    if lama_inap <= 2:
+         total_harga = 200000*lama_inap
+    elif lama_inap <= 4:
+         total_harga = 180000*lama_inap
+    else:
+         total_harga = 160000*lama_inap
+
+    garis()
+    print(" tipe kamar yang di pilih : ", (tipe_kamar))
+    print(" lama menginap : ", (lama_inap), " hari")
+    print(" total harga yang di bayar : rp. ", (total_harga))
+    home = input("ingin memesan lagi? (Y/N) : ")
+
+back = print("")
+if home == "Y":
+    garis()
+    tipe_kamar = input("masukan tipe kamar : ")
+    lama_inap = int(input("masukan lama menginap : "))
+
+    if lama_inap <= 2:
+        total_harga = 100000*lama_inap
+    elif lama_inap <= 4:
+        total_harga = 90000*lama_inap
+    else:
+        total_harga = 80000*lama_inap
+
+    if lama_inap <= 2:
+          total_harga = 150000*lama_inap
+    elif lama_inap <= 4:
+          total_harga = 135000*lama_inap
+    else:
+          total_harga = 120000*lama_inap
+
+    if lama_inap <= 2:
+         total_harga = 200000*lama_inap
+    elif lama_inap <= 4:
+         total_harga = 180000*lama_inap
+    else:
+         total_harga = 160000*lama_inap
+
+    garis()
+    print(" tipe kamar yang di pilih : ", (tipe_kamar))
+    print(" lama menginap : ", (lama_inap), " hari")
+    print(" total harga yang di bayar : rp. ", (total_harga))
+    home = input("ingin memesan lagi? (Y/N) : ")
+
+back = print("")
+if home == "Y":
+    garis()
+    tipe_kamar = input("masukan tipe kamar : ")
+    lama_inap = int(input("masukan lama menginap : "))
+
+    if lama_inap <= 2:
+        total_harga = 100000*lama_inap
+    elif lama_inap <= 4:
+        total_harga = 90000*lama_inap
+    else:
+        total_harga = 80000*lama_inap
+
+    if lama_inap <= 2:
+          total_harga = 150000*lama_inap
+    elif lama_inap <= 4:
+          total_harga = 135000*lama_inap
+    else:
+          total_harga = 120000*lama_inap
+
+    if lama_inap <= 2:
+         total_harga = 200000*lama_inap
+    elif lama_inap <= 4:
+         total_harga = 180000*lama_inap
+    else:
+         total_harga = 160000*lama_inap
+
+    garis()
+    print(" tipe kamar yang di pilih : ", (tipe_kamar))
+    print(" lama menginap : ", (lama_inap), " hari")
+    print(" total harga yang di bayar : rp. ", (total_harga))
+    home = input("ingin memesan lagi? (Y/N) : ")
+else:
+    back
